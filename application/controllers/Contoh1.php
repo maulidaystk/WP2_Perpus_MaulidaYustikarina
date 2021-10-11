8c0359c94709b68d8013db4c5cfed68c87d43aca
maulidaystk <maulidayustikarina20@gmail.com>
2021-10-12 00:13:24 +0700

    added file

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<h3>Nama Saya Maulida Yustikarina,
+         <br>Saya Tinggal Pondok Ungu Permai Blok DD 5 Bekasi
+          <br>Olah Raga Yang di Sukai adalah SepakBola<h3>";
+    }
+}
+?>
+        
+
+
+ 
